24884b0fd9362efdbe54181722e554c1a8bbcf31
Guilherme Lima <glima@noovi.com.br>
2022-01-26 10:02:03 -0300

    Aprendendo Arrays

---
+++ arrayAssociativo.php
@@ -0,0 +1,46 @@
+<?php
+
+$conta1 = [
+    'titular' => 'Vinicius',
+    'saldo' => 1000
+];
+
+$conta2 = [
+    'titular' => 'Maria',
+    'saldo' => 10000
+];
+
+$conta3 = [
+    'titular' => 'Guilherme',
+    'saldo' => 90000
+];
+
+$contasCorrentes = [
+    123654 => [
+        'titular' => 'Vinicius',
+        'saldo' => 1000
+    ],
+    123745 => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    416541 => [
+        'titular' => 'Guilherme',
+        'saldo' => 90000
+    ]
+];
+
+$contasCorrentes[741848] = [
+    
+        'titular' => 'Julia',
+        'saldo' => 90000
+    
+];
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $cpf . " " . $conta['titular'] . '<br>';
+}
+
+/*for($i=0; $i<count($contasCorrentes); $i++) {
+    echo $contasCorrentes[$i]['titular'] . "<br>";
+}*/
